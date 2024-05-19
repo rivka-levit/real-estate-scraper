21eadbc3a276363b8c4162adcf5482a6655ddaad
rivka-yuliia <juliacreadora@gmail.com>
2024-05-19 16:51:07 +0300

    input and output processors functions

--- app/real_estate/items.py
+++ app/real_estate/items.py
@@ -5,9 +5,50 @@
 
 import scrapy
 
+from scrapy.loader.processors import MapCompose
+
+
+def description_in(descr):
+    """
+    Function for input processor.
+
+    Args:
+        descr (str): item received from item loader
+
+    Returns:
+        str: item without spaces
+    """
+
+    return descr.strip()
+
+
+def description_out(descr):
+    """
+    Function for output processor.
+
+    Args:
+        descr (list): item values received and processed by item loader
+
+    Returns:
+        dict: structured item values to output
+    """
+
+    labels = descr[:3]
+    values = descr[3:]
+    output = {
+        labels[0]: ''.join(values[0]),
+        labels[1]: '; '.join(values[1:-1]),
+        labels[2]: ''.join(values[-1])
+    }
+
+    return output
+
 
 class RealEstateItem(scrapy.Item):
     name = scrapy.Field()
-    description = scrapy.Field()
+    description = scrapy.Field(
+        input_processor=MapCompose(description_in),
+        output_processor=description_out
+    )
     price = scrapy.Field()
     agency = scrapy.Field()
